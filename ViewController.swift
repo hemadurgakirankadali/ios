29e8b381f32714eefb0d9a1123595016bf0039b8
hemadurgakirankadali <112112171+hemadurgakirankadali@users.noreply.github.com>
2023-02-28 01:56:09 -0600

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -12,26 +12,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var text: UITextField!
     
     
-    @IBOutlet weak var discount: UITextField!
-    
-    
-    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var discount: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+\
     
     
     
     @IBAction func submit(_ sender: UIButton) {
         
-        var amount = Double(text.text!)
-        var discount = Double(discount.text!)
-        var priceAfterDiscount = amount!-(amount!*discount!/100)
-        display.text = "price After discount :$\(priceAfterDiscount)"
+        
+        
     }
 }
 
