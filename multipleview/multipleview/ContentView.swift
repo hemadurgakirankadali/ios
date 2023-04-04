07bba3cf4829c1aa040aa0268ab0067de26b416d
hemadurgakirankadali <112112171+hemadurgakirankadali@users.noreply.github.com>
2023-04-04 09:23:18 -0500

    Add files via upload

---
+++ multipleview/multipleview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  multipleview
+//
+//  Created by Kadali,Hema Durga Kiran on 4/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ multipleview/multipleview/multipleviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  multipleviewApp.swift
+//  multipleview
+//
+//  Created by Kadali,Hema Durga Kiran on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct multipleviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
