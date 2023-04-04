3466b8cb9962810d860dbba93bdf99d1fc8c1ef8
hemadurgakirankadali <112112171+hemadurgakirankadali@users.noreply.github.com>
2023-04-04 09:03:43 -0500

    Add files via upload

---
+++ CourseDisplayApp/CourseDisplayApp/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  CourseDisplayApp
+//
+//  Created by Kadali,Hema Durga Kiran on 3/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayimage: UIImageView!
+    
+    
+    @IBOutlet weak var crsNum: UILabel!
+    
+    @IBOutlet weak var crsTitle: UILabel!
+    
+    
+    @IBOutlet weak var semOffered: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+    @IBAction func previousbtnclicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextbtnclicked(_ sender: UIButton) {
+    }
+}
+
